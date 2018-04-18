5abaad5e236959a8cd875fd9cebce56646ca18df
Anderson Silva <andersoonn.ams@gmail.com>
2018-04-18 10:31:57 -0300

    addying default tabbar

--- Bazar/App.js
+++ Bazar/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+import Tabbar from 'react-native-tabbar-bottom'
+
+export default class exampleTabs extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      page: "HomeScreen",
+    }
+    
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        {
+          // if you are using react-navigation just pass the navigation object in your components like this:
+          // {this.state.page === "HomeScreen" && <MyComp navigation={this.props.navigation}>Screen1</MyComp>}
+        }
+        {this.state.page === "HomeScreen" && <Text>Screen1</Text>}
+        {this.state.page === "NotificationScreen" && <Text>Screen2</Text>}
+        {this.state.page === "ProfileScreen" && <Text>Screen3</Text>}
+        {this.state.page === "ChatScreen" && <Text>Screen4</Text>}
+        {this.state.page === "SearchScreen" && <Text>Screen5</Text>}
+
+        <Tabbar
+          stateFunc={(tab) => {
+            this.setState({page: tab.page})
+            //this.props.navigation.setParams({tabTitle: tab.title})
+          }}
+          activePage={this.state.page}
+          tabs={[
+            {
+              page: "HomeScreen",
+              icon: "home"
+
+            },
+            {
+              page: "NotificationScreen",
+              icon: "notifications",            
+            },
+            {
+              page: "ProfileScreen",
+              icon: "person",
+            },
+            {
+              page: "ChatScreen",
+              icon: "ios-add",
+            },
+          ]}
+        />
       </View>
     );
   }
@@ -15,9 +58,6 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
